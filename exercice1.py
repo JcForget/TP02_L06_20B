a2a994742737d7b54609403f8b68a833f4e9fa8f
JC <forgeje@gmail.com>
2021-09-30 19:20:32 -0400

    fonctionnent

--- exercice1.py
+++ exercice1.py
@@ -1,9 +1,30 @@
 def combine_dic(dic_1, dic_2):
-    # TODO Compléter la fonction afin de combiner dic_1 et dic_2
-    #      en gardant la valeur max en cas de clef commune
-    dic_3 = ...
+    # Compléter la fonction afin de combiner dic_1 et dic_2
+    #     en gardant la valeur max en cas de clef commune
+    copie_de_dic2 = dic_2
+    réponse = {}
 
-    return dic_3
+    for clé in dic_1.keys():
+        # si la clef est dans les deux dictionnaires
+        if clé in dic_2:
+            # une liste avec les deux valeurs
+            valeur = [dic_1.get(clé),dic_2.get(clé)]
+            # ajoute la plus grande valeur et la clé à la réponse
+            réponse.update({clé:max(valeur)})
+            # supprime le doublon de la deuxième matrice
+            copie_de_dic2.pop(clé)
+        # si la clef est seulement dans le dictionnaire 1
+        else:
+            # Ajoute la clef dans la réponse
+            réponse.update({clé:dic_1.get(clé)})
+    # finalement, ajoute les clefs restantes du deuxième dictionnaire.
+    réponse.update(copie_de_dic2)
+
+
+
+
+
+    return réponse
 
 if __name__ == '__main__':
     # Combinaison de dictionnaire
--- exercice2.py
+++ exercice2.py
@@ -1,5 +1,14 @@
 def tri_bulle(tab):
-    # TODO écrire l'algorithme du tri à bulle comme décrit dans l'énoncé
+    #  écrire l'algorithme du tri à bulle comme décrit dans l'énoncé
+    # défini la grandeur du tableau
+    longueur = len(tab)
+    # parcourir la liste en ordre décroissant
+    for i in range(longueur,1,-1):
+        for j in range (i-1):
+            if tab[j+1] < tab[j]:
+                memoire = tab[j+1]
+                tab[j+1] = tab[j]
+                tab[j] = memoire
 
     return tab
 
